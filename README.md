86d97c08a7eaac2bd46b00562b554b82349e8e0f
CHRISTIAN EDUARDO NAVARRO CURIEL <1724110319@utectulancingo.edu.mx>
2025-11-06 17:24:31 +0000

    Actualizacion 04_web

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+
 # mvc_hola_mundo
 Este repositorio es para aprender la estructura MVC
 
---
+++ webapp/04_web/static/js/script.js
@@ -0,0 +1,11 @@
+//1. Obtener los elementos del DOM usando sus IDs.
+const boton = document.getElementById('agregarActividad')
+
+boton.addEventListener('click',function() {
+    const input = document.getElementById('actividad');
+    const lista = document.getElementById('listaActividades');
+    const text = input.value.trim();
+    const nuevaActividad = document.createElement('li');
+    nuevaActividad.textContent = text;
+    lista.appendChild(nuevaActividad);
+});
